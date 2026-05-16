c6c8f2eeeda2bd32db00c1b1546d55b5b3fd0d73
tgkuo <tgkuo1@gmail.com>
2026-05-16 10:21:49 +0800

    fix: engines compatible with local Node 22

--- svelte.config.js
+++ svelte.config.js
@@ -2,20 +2,17 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', '.md', '.svx'],
-
   preprocess: [
     vitePreprocess(),
-    mdsvex({
-      extensions: ['.md', '.svx'],
-    }),
+    mdsvex({ extensions: ['.md', '.svx'] })
   ],
-
   kit: {
-    adapter: adapter(),
-  },
+    adapter: adapter({
+      runtime: 'nodejs20.x'
+    })
+  }
 };
 
 export default config;
