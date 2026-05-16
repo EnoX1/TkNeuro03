e217cfa1cb9ad992515167f1b3cab31b6617795c
tgkuo <tgkuo1@gmail.com>
2026-05-16 11:06:21 +0800

    fix: use adapter-vercel with nodejs20.x runtime

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { mdsvex } from "mdsvex";
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 const config = {
-  extensions: [".svelte", ".md", ".svx"],
-  preprocess: [vitePreprocess(), mdsvex({ extensions: [".md", ".svx"] })],
+  extensions: ['.svelte', '.md', '.svx'],
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({ extensions: ['.md', '.svx'] })
+  ],
   kit: {
     adapter: adapter({
-      runtime: "nodejs20.x",
-    }),
-  },
+      runtime: 'nodejs20.x'
+    })
+  }
 };
 
 export default config;
