5d11990a64addc80d2da98a968d53b314bd30a34
tgkuo <tgkuo1@gmail.com>
2026-05-30 16:02:13 +0800

    P5.js

---
+++ src/routes/viz/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import P5 from 'p5-svelte';
+
+  const sketch = (p) => {
+    p.setup = () => {
+      p.createCanvas(600, 400);
+      p.background(20);
+    };
+    p.draw = () => {
+      p.background(20, 20, 40);
+      p.fill(100, 200, 255);
+      p.noStroke();
+      p.ellipse(p.mouseX, p.mouseY, 30, 30);
+    };
+  };
+</script>
+
+<h1 class="text-2xl font-bold p-4">TkNeuro Viz</h1>
+<P5 {sketch} />
